141f7e07756e3c0c17bc24014dd3e8094aec6dea
Luis Amez <luis.amez.vitoria@gmail.com>
2018-03-22 00:09:56 +0100

    Add route to retrieve clients

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,5 +1,24 @@
 // API Controllers
 
+const request = require('request-promise-native');
+
+// Default call
 exports.default = (req, res) => {
   res.status(200).json({ message: 'ok' });
+};
+
+// List of clients
+exports.clients = (req, res, next) => {
+  const options = {
+    uri: process.env.CLIENTS_URI,
+    json: true
+  };
+
+  request(options)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      next(error);
+    });
 };
--- routes/api.js
+++ routes/api.js
@@ -5,5 +5,6 @@ const apiController = require('../controllers/apiController');
 
 // 'ok' response
 router.get('/', apiController.default);
+router.get('/clients', apiController.clients);
 
 module.exports = router;
