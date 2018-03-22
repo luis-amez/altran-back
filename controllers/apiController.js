2add34d3be0e28cc86dfdc739923b3eb564f5ce3
Luis Amez <luis.amez.vitoria@gmail.com>
2018-03-22 20:09:27 +0100

    Add endpoint for retrieving users data by name

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -50,3 +50,30 @@ exports.userById = (req, res, next) => {
       next(error);
     });
 };
+
+// Users data by username
+exports.usersByName = (req, res, next) => {
+  const userName = req.params.name;
+
+  const options = {
+    uri: process.env.CLIENTS_URI,
+    method: 'GET',
+    json: true
+  };
+
+  request(options)
+    .then(response => {
+      const user = response.clients.filter(client => {
+        return client.name === userName;
+      });
+
+      if (user.length > 0) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+};
--- routes/api.js
+++ routes/api.js
@@ -7,5 +7,6 @@ const authController = require('../controllers/authController');
 router.get('/', authController.isUser, apiController.default);
 router.get('/clients', apiController.clients);
 router.get('/user-by-id/:id', authController.isUser, apiController.userById);
+router.get('/users-by-name/:name', authController.isUser, apiController.usersByName);
 
 module.exports = router;
