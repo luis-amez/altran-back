ddaec9c30f1d0c3218749720be6fa6422fd097a7
Luis Amez <luis.amez.vitoria@gmail.com>
2018-03-23 13:03:51 +0100

    Add endpoint for retrieving user by policy id

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -124,3 +124,50 @@ exports.policiesByUserName = (req, res, next) => {
       next(error);
     });
 };
+
+// User linked to policy number
+exports.userByPolicyId = (req, res, next) => {
+  const policyId = req.params.id;
+
+  const optionsPoliciesRequest = {
+    uri: process.env.POLICIES_URI,
+    method: 'GET',
+    json: true
+  };
+
+  request(optionsPoliciesRequest)
+    .then(responsePolicy => {
+      const policy = responsePolicy.policies.find(policyElem => {
+        return policyElem.id === policyId;
+      });
+
+      if (policy) {
+        const optionsUserRequest = {
+          uri: process.env.CLIENTS_URI,
+          method: 'GET',
+          json: true
+        };
+
+        request(optionsUserRequest)
+          .then(responseUser => {
+            const user = responseUser.clients.find(client => {
+              return client.email === policy.email;
+            });
+
+            if (user) {
+              res.status(200).json(user);
+            } else {
+              res.status(404).json({ message: 'This policy is linked to a non-existent user' });
+            }
+          })
+          .catch(error => {
+            next(error);
+          });
+      } else {
+        res.status(404).json({ message: 'Policy not found' });
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+};
--- routes/api.js
+++ routes/api.js
@@ -9,5 +9,6 @@ router.get('/clients', apiController.clients);
 router.get('/user-by-id/:id', authController.isUser, apiController.userById);
 router.get('/users-by-name/:name', authController.isUser, apiController.usersByName);
 router.get('/policies-by-name/:name', authController.isAdmin, apiController.policiesByUserName);
+router.get('/user-by-policy-id/:id', authController.isAdmin, apiController.userByPolicyId);
 
 module.exports = router;
